00534a64ea42abf1e77da3c494e113a0338fc15d
Daniel <danielring13@gmail.com>
2024-02-27 09:02:36 +0100

    v9

---
+++ js/js.js
@@ -0,0 +1,7 @@
+// let button = document.querySelector(".menu-button")
+// button.addEventListener("click", showMenu)
+
+function changeMenu(){
+    let menu = document.querySelector("nav.menu")
+    menu.classList.toggle("show")
+}
