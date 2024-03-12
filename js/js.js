22e5349ad0485acd74db5689c65e2230ef3ef87c
Daniel <danielring13@gmail.com>
2024-03-12 10:29:11 +0100

    2

--- js/js.js
+++ js/js.js
@@ -1,7 +1,11 @@
-let button = document.querySelector(".menu-button")
-// button.addEventListener("click", showMenu)
+// let button = document.querySelector(".menu-button")
+// button.addEventListener("click", changeMenu)
+// let layer = document.querySelector(".fade-layer")
+// layer.addEventListener("click", changeMenu)
 
 function changeMenu(){
     let menu = document.querySelector("nav.menu")
     menu.classList.toggle("show")
+    let layer = document.querySelector(".fade-layer")
+    layer.classList.toggle("visible")
 }
